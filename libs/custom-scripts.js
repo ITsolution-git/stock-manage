d5fc09a6b5cf2b5ce7aabb495d51b1df513d05b5
codal-hirenUI <hparmar@codal.com>
2015-11-03 16:44:20 +0530

    Order Nav --> Position Tab styling commit.

--- libs/custom-scripts.js
+++ libs/custom-scripts.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
-	
+(function($) {
 	// ADMIN - MISC View Scroll
 	var scroll_container = $('#misc-view');
 	$(scroll_container).jScrollPane();
-});
+
+	// Scroll on order menu - Position Tab
+	$('.client_order_form #orderTab_scroll').jScrollPane();
+})(jQuery);
