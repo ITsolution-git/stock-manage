600ae1e44754d056f34271cc1ec54813db462983
kjoshi <kjoshi@codal.com>
2015-12-10 12:20:01 +0530

    Fetching company detail code change.

--- scripts/app.ctrl.js
+++ scripts/app.ctrl.js
@@ -22,24 +22,24 @@ angular.module('app')
      // console.log(sessionService.get('role_slug'));
       if(sessionService.get('role_slug')!='SA' && sessionService.get('role_slug')!='' && sessionService.get('role_slug')!=null)
       {
-      var user_data = {};
-      user_data.user_id=sessionService.get('user_id');
-      $http.post('api/public/auth/company',user_data).then(function successCallback(Response) {
-       // console.log(Response.data.data.success);
-          if(Response.data.data.success=='1')
-          {
-              $rootScope.company_profile = Response.data.data.records;
-          }
-          else
-          {
-              var data = {"status": "error", "message": "Company not assigned for this User!"}
-              notifyService.notify(data.status, data.message);
-              $state.go('access.lockme');
-              return false;
-          }
-        }, function errorCallback(user_data) {
+          var user_data = {};
+          user_data.user_id=sessionService.get('user_id');
+          $http.post('api/public/auth/company',user_data).then(function successCallback(Response) {
+              // console.log(Response.data.data.success);
+              if(Response.data.data.success=='1')
+              {
+                  $rootScope.company_profile = Response.data.data.records;
+              }
+              else
+              {
+                  var data = {"status": "error", "message": "Company not assigned!"}
+                  notifyService.notify(data.status, data.message);
+                  $state.go('access.lockme');
+                  return false;
+              }
+          }, function errorCallback(user_data) {
              console.log('error');
-        });
+          });
       }
       /*==============================*/
 
