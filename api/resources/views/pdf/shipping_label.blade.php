a57e3d81b87a0c4bed4537894caf23f449c82a5f
codal-hirenUI <hparmar@codal.com>
2016-01-18 15:32:42 +0530

    Commit: - Shipping --> HTML Development for PDF page            --> Shipping Label

--- api/resources/views/pdf/shipping_label.blade.php
+++ api/resources/views/pdf/shipping_label.blade.php
@@ -3,31 +3,24 @@
 <head>
   <meta charset="utf-8">
   <meta name="description" content="">
-  <title>Orders Print</title>
+  <title>Shipping Label</title>
 
   <style type="text/css">
     body {
       margin: 0;
       padding: 0;
     }
-    div.prntBrdr {
-      border:1px solid #000000;
-      border-radius: 4px;
-      padding-top:15px;
+    div.boxbrdr {
+      border:2px solid #000000;
+      color: #000000;
+      font-size: 70px;
+      margin-top: 10px;
+      padding-top:70px;
       padding-right:15px;
       padding-left:15px;
-      padding-bottom:15px;
-      text-align: left;    
-    }
-
-    div.prntBrdrBox {
-      border:1px solid #000000;      
-      height: 30px; 
-      width: 45px;
-    }
-    .brdrBox {
-      border:1px solid #000000;
-      height: 18px;
+      text-align: center;       
+      width: 100%;
+      height: 50px;
     }
     h1, h2, h3, h4, h5, h6 {
       margin: 0;
@@ -39,13 +32,16 @@
       padding: 0;
     }
     th {
-      font-size: 12px;
+      font-size: 22px;
+      height: 30px;
+      line-height: 19px;
+      padding: 5px;
     }
     td {
-      font-size: 14px;        
+      font-size: 16px;
     }
-    .evenRow {
-      background: #e5e5e5;
+    td.topbrdr {
+      border-top:1px solid #999999;
     }
     p {
       margin: 0;
@@ -53,12 +49,15 @@
       font-size: 14px;
       line-height: 19px;
     }
-    p.txtUpCash {
-      font-size: 14px;
-      text-transform: uppercase;
+    thead.title th {
+      padding: 5px 0;
+      border-bottom: 1px solid #999999;
     }
-    p.txtSmall {
-      font-size: 10px;
+    tbody.color-grey td {
+      border:0 none;
+      line-height: 19px;
+      height: 20px;
+      padding: 10px 0;
     }
   </style>
 </head>
@@ -76,9 +75,8 @@
               <p>www.url.com</p>
             </td>
             <td align="right" valign="top" width="50%">
-              <p><strong>Estimate #123</strong></p>
-              <p><strong>Created On: </strong></p>
-              <p><strong>Job Name:  </strong></p>
+              <p><strong>Divinity Seven</strong></p>
+              <p><strong>Scott Witherspoon</strong></p>
             </td>
           </tr>
           <tr>
@@ -89,362 +87,35 @@
     </tr>
 
     <tr>
-      <td align="left" valign="top" width="100%">          
+      <td align="left" valign="top" width="100%">PO : Crafted for conscioun</td>
+    </tr>
+    
+    <tr>
+      <td align="left" valign="top" width="100%">        
         <table border="0" cellpadding="0" cellspacing="0" width="100%">
-          <tr>
-            <td align="left" valign="top" width="49%">
-              <div class="prntBrdr">
-                <h2>BILL TO</h2>
-                <p><strong>Misano Salon</strong></p>
-                <p>justina kowalzyk</p>
-              </div>
-            </td>
-            <td align="left" valign="top" width="2%">&nbsp;</td>
-            <td align="right" valign="top" width="49%">
-              <div class="prntBrdr">
-                <h2>SHIP TO</h2>
-                <p><strong>Misano Salon</strong></p>
-                <p>justina kowalzyk</p>
-              </div>
-            </td>
-          </tr>
+            <thead class="title">
+              <tr>
+                <th align="left" valign="top" width="49%">Description</th>
+                <th align="left" valign="top" width="2%">&nbsp;</th>
+                <th align="right" valign="top" width="49%">Color</th>
+              </tr>
+            </thead>
+            <tbody class="color-grey">
+              <tr>
+                <td align="left" valign="top" width="49%" height="10">
+                  crewneck sweatshirts
+                  <div class="boxbrdr">3</div>
+                </td>
+                <td align="left" valign="top" width="2%">&nbsp;</td>
+                <td align="right" valign="top" width="49%" height="10">
+                  Grey
+                  <div class="boxbrdr" style="float:right;">M</div>
+                </td>
+              </tr>
+            </tbody>
         </table>
       </td>
     </tr>
-
-    <tr>
-      <td align="left" valign="top" width="100%">&nbsp;</td>
-    </tr>
-
-    <tr>
-        <td align="left" valign="top" width="100%">        
-          <table border="0" cellpadding="0" cellspacing="0" width="100%">
-              <thead>
-                <tr>
-                  <th align="left" valign="top" width="20%">Customer PO</th>
-                  <th align="left" valign="top" width="20%">Rep/Brand Co.</th>
-                  <th align="left" valign="top" width="10%">Terms</th>
-                  <th align="left" valign="top" width="10%">Ship Via</th>
-                  <th align="left" valign="top" width="10%">Ship Date</th>
-                  <th align="left" valign="top" width="20%">In Hands Date</th>
-                  <th align="left" valign="top" width="20%">Payment Due</th>
-                </tr>
-                <tr>
-                  <th colspan="7">&nbsp;</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td align="left" valign="top" class="brdrBox" width="20%"></td>
-                  <td align="left" valign="top" class="brdrBox" width="20%">&nbsp;Dark Room</td>
-                  <td align="left" valign="top" class="brdrBox" width="10%"></td>
-                  <td align="left" valign="top" class="brdrBox" width="10%"></td>
-                  <td align="left" valign="top" class="brdrBox" width="10%"></td>
-                  <td align="left" valign="top" class="brdrBox" width="20%"></td>
-                  <td align="left" valign="top" class="brdrBox" width="20%"></td>
-                </tr>
-              </tbody>
-          </table>
-        </td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">&nbsp;</td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">        
-          <table border="0" cellpadding="0" cellspacing="0" width="100%" class="garmentDetails">
-            <thead>
-              <tr>
-                <th align="left" valign="top" width="35%">Garment / Item Description</th>
-                <th align="left" valign="top" width="10%">Color</th>
-                <th align="left" valign="top" width="30%">Sizes / Quantities</th>
-                <th align="left" valign="top" width="5%">OS</th>
-                <th align="left" valign="top" width="10%">Qnty</th>
-                <th align="left" valign="top" width="10%">Unit Price</th>                
-              </tr>
-              <tr>
-                <th colspan="6">&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td align="left" valign="top" class="brdrBox" width="35%">&nbsp;Garments 1</td>
-                <td align="left" valign="top" class="brdrBox" width="10%">&nbsp;Black</td>
-                <td align="left" valign="top" class="brdrBox" width="30%">&nbsp;S:(50) L:(50)</td>
-                <td align="left" valign="top" class="brdrBox" width="5%">&nbsp;2</td>
-                <td align="left" valign="top" class="brdrBox" width="10%">&nbsp;100</td>
-                <td align="left" valign="top" class="brdrBox" width="10%">&nbsp;10.00</td>
-              </tr>
-
-              <tr class="evenRow">
-                <td align="left" valign="top" class="brdrBox" width="35%">&nbsp;Garments 1</td>
-                <td align="left" valign="top" class="brdrBox" width="10%">&nbsp;Black</td>
-                <td align="left" valign="top" class="brdrBox" width="30%">&nbsp;S:(50) L:(50)</td>
-                <td align="left" valign="top" class="brdrBox" width="5%">&nbsp;2</td>
-                <td align="left" valign="top" class="brdrBox" width="10%">&nbsp;100</td>
-                <td align="left" valign="top" class="brdrBox" width="10%">&nbsp;10.00</td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">&nbsp;</td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">          
-          <table border="0" cellpadding="0" cellspacing="0" width="100%">
-            <tr>
-              <td align="left" valign="top" width="25%">
-                <div class="payDetails">                  
-                  <p>Some Other Text</p>
-                  <p>&nbsp;</p>
-                </div>
-
-              
-              </td>
-              <td align="left" valign="top" width="50%">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                  <tr>
-                    <td align="left" valign="top" width="49%">
-                      <img src="" alt="Image 1" style="display:block; max-width:100%;" />    
-                    </td>
-                    <td align="left" valign="top" width="2%">&nbsp;</td>
-                    <td align="left" valign="top" width="49%">
-                      <img src="" alt="Image 1" style="display:block; max-width:100%;" />
-                    </td>
-                  </tr>
-                </table>
-              </td>
-              <td align="right" valign="top" width="25%">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                  <tr>
-                    <td align="right" valign="top" width="45%">Total Qnty</td>
-                    <td align="left" valign="top" width="5%">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox" width="50%">308&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Screens</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">2,775.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Press Setup</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">0.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Digitize</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">6.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Art Work</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">8.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Separations</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">6.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Rush</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">2.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Distribution</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">4.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Shipping</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">3.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">new lab</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">6.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Discount</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">10.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Order Total</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">5,859.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Tax</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">542.31&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Grand Total</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">6,401.95&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Payments/Deposit</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">6.00&nbsp;</td>
-                  </tr>
-
-                  <tr>
-                    <td align="right" valign="top">Balance Due</td>
-                    <td align="left" valign="top">&nbsp;</td>
-                    <td align="right" valign="top" class="brdrBox">6,401.95&nbsp;</td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">        
-          <table border="0" cellpadding="0" cellspacing="0" width="100%">          
-              <tr>
-                <th align="left" valign="top" width="30%">Garment Link</th>
-                <th align="left" valign="top" width="70%">NOTES</th>
-              </tr>
-
-              <tr>
-                <td align="left" valign="top" width="30%">
-                  <a href="#"></a>
-                </td>
-                <td align="left" valign="top" width="70%">
-                  <textarea class="brdrBox" rows="10"></textarea>
-                </td>
-              </tr>          
-          </table>
-        </td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">&nbsp;</td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">        
-          <table border="0" cellpadding="0" cellspacing="0" width="35%" align="center">
-            <thead>
-              <tr>
-                <th align="center" valign="top" width="23%">Production QC</th>
-                <th align="center" valign="top" width="2%">&nbsp;</th>
-                <th align="center" valign="top" width="23%">QC</th>
-                <th align="center" valign="top" width="2%">&nbsp;</th>
-                <th align="center" valign="top" width="23%">Pack</th>
-                <th align="center" valign="top" width="2%">&nbsp;</th>
-                <th align="center" valign="top" width="23%">Ship</th>
-                <th align="center" valign="top" width="2%">&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td align="center" valign="top">
-                  <div class="prntBrdrBox"></div>
-                </td>
-                <td align="center" valign="top">&nbsp;</td>
-                <td align="center" valign="top">
-                  <div class="prntBrdrBox"></div>
-                </td>
-                <td align="center" valign="top">&nbsp;</td>
-                <td align="center" valign="top">
-                  <div class="prntBrdrBox"></div>
-                </td>
-                <td align="center" valign="top">&nbsp;</td>
-                <td align="center" valign="top">
-                  <div class="prntBrdrBox"></div>
-                </td>
-                <td align="center" valign="top">&nbsp;</td>
-              </tr>
-            </tbody>    
-          </table>
-        </td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">&nbsp;</td>
-      </tr>
-
-      <tr>
-        <td align="left" valign="top" width="100%">        
-          <table border="0" cellpadding="0" cellspacing="0" width="35%" align="center">
-            <thead>
-              <tr>
-                <th align="center" valign="top" width="23%">Production QC</th>
-                <th align="center" valign="top" width="2%">&nbsp;</th>
-                <th align="center" valign="top" width="23%">QC</th>
-                <th align="center" valign="top" width="2%">&nbsp;</th>
-                <th align="center" valign="top" width="23%">Pack</th>
-                <th align="center" valign="top" width="2%">&nbsp;</th>
-                <th align="center" valign="top" width="23%">Ship</th>
-                <th align="center" valign="top" width="2%">&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td align="center" valign="top">
-                  <div class="prntBrdrBox"></div>
-                </td>
-                <td align="center" valign="top">&nbsp;</td>
-                <td align="center" valign="top">
-                  <div class="prntBrdrBox"></div>
-                </td>
-                <td align="center" valign="top">&nbsp;</td>
-                <td align="center" valign="top">
-                  <div class="prntBrdrBox"></div>
-                </td>
-                <td align="center" valign="top">&nbsp;</td>
-                <td align="center" valign="top">
-                  <div class="prntBrdrBox"></div>
-                </td>
-              </tr>
-            </tbody>    
-          </table>
-        </td>
-      </tr>
-
-      <tr>
-        <td align="center" valign="top" width="100%">        
-          <table border="0" cellpadding="0" cellspacing="0" width="100%">
-              <tr>
-                <td align="center" valign="top">&nbsp;</td>
-              </tr>                     
-              <tr>
-                <td align="center" valign="top">
-                  <p class="txtUpCash">Please respond with "Approved, Name and Date",  for order to be placed into production</p>
-                  <p class="txtSmall">Unless document states final invoice the amount listed may not be the total due. Shipping, tax and any additions during the art or press stages may result in a change of price.</p>
-                </td>
-              </tr>
-          </table>
-        </td>
-      </tr>
-
 </table>
 </body>
 </html>
