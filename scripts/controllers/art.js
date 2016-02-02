a86c23e930c2753a908ba82b361128fdb8eb134d
kjoshi <kjoshi@codal.com>
2016-02-02 17:24:21 +0530

    Order line and Notes tab code changed and DB changes.

--- scripts/controllers/art.js
+++ scripts/controllers/art.js
@@ -11,7 +11,7 @@ app.controller('ArtListCtrl', ['$scope',  '$http','$state','$stateParams','$root
                             });
 
 }]);
-app.controller('ArtJobCtrl', ['$scope',  '$http','$state','$stateParams','$rootScope', 'AuthService',function($scope,$http,$state,$stateParams,$rootScope,AuthService) {
+app.controller('ArtJobCtrl', ['$scope',  '$http','$state','$stateParams','$rootScope', 'AuthService','notifyService',function($scope,$http,$state,$stateParams,$rootScope,AuthService,notifyService) {
                           AuthService.AccessService('BC');
                           $scope.CurrentController=$state.current.controller;
                           $scope.company_id = $rootScope.company_profile.company_id;
@@ -19,9 +19,29 @@ app.controller('ArtJobCtrl', ['$scope',  '$http','$state','$stateParams','$rootS
                           $http.get('api/public/art/Art_detail/'+$scope.art_id+'/'+$scope.company_id).success(function(RetArray) {
 	                          	if(RetArray.data.success=='1')
                           		{
-                          			$scope.Artjob_position = RetArray.data;
+                          			$scope.art_position = RetArray.data.records.art_position;
+                          			$scope.art_orderline = RetArray.data.records.art_orderline;
+                          			//console.log($scope.art_orderline.line_array);
                               	}
                             });
+                           $scope.getNumber = function(num) {
+							    return new Array(num);   
+							}
+
+						  $scope.UpdateField_detail = function($event,id){
+                          		  var Receive_data = {};
+                          		  Receive_data.table ='art';
+                          		  $scope.name_filed = $event.target.name;
+                          		  var obj = {};
+                          		  obj[$scope.name_filed] =  $event.target.value;
+                          		  Receive_data.data = angular.copy(obj);
+                          		  
+	                              Receive_data.cond ={ art_id :id}
+	                              $http.post('api/public/common/UpdateTableRecords',Receive_data).success(function(result) {
+	                              		var data = {"status": "success", "message": "Data Updated successfully"}
+                                        notifyService.notify(data.status, data.message); 
+                                });
+                          }
 
 }]);
 
