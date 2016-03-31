4dedab6e14b0b175b8bc8553606631fd33e29397
kjoshi <kjoshi@codal.com>
2016-03-31 16:18:35 +0530

    Theme setting, Chat Nav, language Nav removed, Site title set in Home page and Loading Image.

--- scripts/app.js
+++ scripts/app.js
@@ -116,13 +116,13 @@
     function config($translatePartialLoaderProvider, msApiProvider)
     {
         // Translation
-        $translatePartialLoaderProvider.addPart('app/quick-panel');
+        //$translatePartialLoaderProvider.addPart('app/quick-panel');
 
         // Api
-        msApiProvider.register('quickPanel.activities', ['app/data/quick-panel/activities.json']);
-        msApiProvider.register('quickPanel.contacts', ['app/data/quick-panel/contacts.json']);
-        msApiProvider.register('quickPanel.events', ['app/data/quick-panel/events.json']);
-        msApiProvider.register('quickPanel.notes', ['app/data/quick-panel/notes.json']);
+        //msApiProvider.register('quickPanel.activities', ['app/data/quick-panel/activities.json']);
+        //msApiProvider.register('quickPanel.contacts', ['app/data/quick-panel/contacts.json']);
+        //msApiProvider.register('quickPanel.events', ['app/data/quick-panel/events.json']);
+        //msApiProvider.register('quickPanel.notes', ['app/data/quick-panel/notes.json']);
     }
 })();
 
