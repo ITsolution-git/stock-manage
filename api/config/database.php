5018afea145592e0a25081c8366c0f04f84888d0
Ram Krishna <rkrishna@codal.com>
2016-04-07 11:52:35 +0530

    Database connection changed back

--- api/config/database.php
+++ api/config/database.php
@@ -52,29 +52,29 @@ return [
 			'prefix'   => '',
 		],
 
-//		'mysql' => [
-//			'driver'    => 'mysql',
-//			'host'      => env('DB_HOST', '192.168.1.13'),
-//			'database'  => env('DB_DATABASE', 'cs'),
-//			'username'  => env('DB_USERNAME', 'csuser'),
-//			'password'  => env('DB_PASSWORD', 'codal123'),
-//			'charset'   => 'utf8',
-//			'collation' => 'utf8_unicode_ci',
-//			'prefix'    => '',
-//			'strict'    => false,
-//		],
-
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
+			'host'      => env('DB_HOST', '192.168.1.13'),
 			'database'  => env('DB_DATABASE', 'cs'),
-			'username'  => env('DB_USERNAME', 'root'),
-			'password'  => env('DB_PASSWORD', ''),
+			'username'  => env('DB_USERNAME', 'csuser'),
+			'password'  => env('DB_PASSWORD', 'codal123'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
 			'strict'    => false,
 		],
+
+//		'mysql' => [
+//			'driver'    => 'mysql',
+//			'host'      => env('DB_HOST', 'localhost'),
+//			'database'  => env('DB_DATABASE', 'cs'),
+//			'username'  => env('DB_USERNAME', 'root'),
+//			'password'  => env('DB_PASSWORD', ''),
+//			'charset'   => 'utf8',
+//			'collation' => 'utf8_unicode_ci',
+//			'prefix'    => '',
+//			'strict'    => false,
+//		],
 
 		'pgsql' => [
 			'driver'   => 'pgsql',
