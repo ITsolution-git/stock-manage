05c0085594f01c7f387a20e53580069422517797
tshobhasana <tshobhasana@codal.com>
2016-05-18 19:17:16 +0530

    new page - recevingInfo

---
+++ app/main/receiving/views/receivingInfo/receivingInfo.controller.js
@@ -0,0 +1,19 @@
+(function ()
+{
+    'use strict';
+
+    angular
+            .module('app.receiving')
+            .controller('ReceivingInfoController', ReceivingInfoController);
+            
+
+    /** @ngInject */
+    function ReceivingInfoController($document, $window, $timeout, $mdDialog,$stateParams,sessionService,$http,$scope,$state)
+    {
+
+        
+        
+    }
+    
+    
+})();
