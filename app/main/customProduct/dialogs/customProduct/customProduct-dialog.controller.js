d3b3e579e41a1bbbbfc20a3edb69051b089020a9
hdeliwala <hdeliwala@codal.com>
2016-05-27 13:17:49 +0530

    STO-182 #comment start add product with change functionality  #time 2h start add product with change functionality

--- app/main/customProduct/dialogs/customProduct/customProduct-dialog.controller.js
+++ app/main/customProduct/dialogs/customProduct/customProduct-dialog.controller.js
@@ -9,11 +9,7 @@
     function CustomProductDialogController(product_id,$scope, $mdDialog, $document, $mdSidenav, DTOptionsBuilder, DTColumnBuilder,$resource,$http,notifyService,$state,sessionService)
     {
 
-       $scope.example6model = [{id: 1}, {id: 3}]; $scope.example6data = [ {id: 1, label: "David"}, {id: 2, label: "Jhon"}, {id: 3, label: "Danny"}]; $scope.example6settings = {};
-
-        /*$scope.colorsettings = {displayProp: 'name', idProp: 'id',enableSearch: true, scrollableHeight: '400px',showCheckAll:false,showUncheckAll:false,scrollable: true};
-        $scope.colorcustomTexts = {buttonDefaultText: 'Select Colors'};
-
+      
         if(product_id == 0) {
 
             var product_data = {};
@@ -30,7 +26,7 @@
             product_data.table ='products'
 
             $http.post('api/public/common/InsertRecords',product_data).success(function(result) {
-                console.log(result.data.id);
+               
                 var id = result.data.id;
                 
                 getProductDetailByIdAll(id);
@@ -52,39 +48,16 @@
         }
 
 
-        get_color_data();
- 
-           $scope.colorEvents = {
-                        onItemSelect: function(item) {
-                            
-                                var color_data = {};
-                                color_data.color_id = item.id;
-                                color_data.product_id = $scope.product_id_new;
-
-                                $http.post('api/public/order/saveColorSize',color_data).success(function(Listdata) {
-                                    getProductDetailByIdAll($scope.product_id_new);
-                                });
-                        },
-                        onItemDeselect: function(item) {
-                          
-                               var color_data = {};
-                                color_data.color_id = item.id;
-                                color_data.product_id = $scope.product_id_new;
-
-                                $http.post('api/public/order/deleteColorSize',color_data).success(function(Listdata) {
-                                    getProductDetailByIdAll($scope.product_id_new);
-                                });
-                        } 
-                        
-             };
-
 
 
          function getProductDetailByIdAll(id)
           {
+             
+             var product_arr = {}
               product_arr = {'id':id};
               $http.post('api/public/order/productDetail',product_arr).success(function(result) {
                       $scope.allProductColorSize =result.data;
+                      $scope.productDetail =result.data.product_data[0];
                    
                        if($scope.allProductColorSize.colorData.length == '0'){
                         $scope.allProductColorSize.colorData = [];
@@ -93,48 +66,30 @@
               }
 
 
-    function getProductDetailColorSize(product_id)
-    {
-        $("#ajax_loader").show();
-        $http.get('api/public/order/getProductDetailColorSize/'+product_id).success(function(result, status, headers, config) 
-        {
-            if(result.data.success == '1') 
-            {
-                $scope.allProductColorSize =result.data.records;
-            } 
-            else
-            {
-                $scope.allProductColorSize=[];
-                $scope.allProductColorSize.productColorSizeData=[];
-                $scope.allProductColorSize.ColorData=[];
-            }
-            $("#ajax_loader").hide();
-        });
-    }
 
 
-
-    function get_color_data()
-    {
-        var colorData = {};
-        colorData.table ='color'
-        colorData.cond ={status:1,is_delete:1}
-        
-        $http.post('api/public/common/GetTableRecords',colorData).success(function(result) {
-            
-            if(result.data.success == '1') 
-            {
-                $scope.allColor =result.data.records;
-            } 
-            else
-            {
-                $scope.allColor=[];
-            }
-        });
-     }*/
+    
 
         $scope.cancel = function () {
             $mdDialog.hide();
         };
+
+        $scope.updateProduct = function(column_name,id,value,table_name,match_condition)
+        {
+            var position_main_data = {};
+            position_main_data.table =table_name;
+            $scope.name_filed = column_name;
+          
+            var obj = {};
+            obj[$scope.name_filed] =  value;
+            position_main_data.data = angular.copy(obj);
+
+            var condition_obj = {};
+            condition_obj[match_condition] =  id;
+            position_main_data.cond = angular.copy(condition_obj);
+
+            $http.post('api/public/common/UpdateTableRecords',position_main_data).success(function(result) {
+            });
+        }
     }
 })();
