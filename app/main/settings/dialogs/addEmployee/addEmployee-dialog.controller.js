a3e05ed0a8c9f68f5f75bb7966e08c7d17e31fed
tshobhasana <tshobhasana@codal.com>
2016-05-30 17:29:07 +0530

    Admin Panel

---
+++ app/main/settings/dialogs/addEmployee/addEmployee-dialog.controller.js
@@ -0,0 +1,30 @@
+(function ()
+{
+    'use strict';
+
+    angular
+        .module('app.settings')
+        .controller('AddEmployeeDialogController', AddEmployeeDialogController);
+
+    /** @ngInject */
+    function AddEmployeeDialogController($mdDialog,$controller,$state, event,$scope,sessionService,$resource)
+    {
+        var vm = this;
+
+        //////////
+
+
+        $scope.cancel = function () {
+            $mdDialog.hide();
+        };
+
+        
+        /**
+         * Close dialog
+         */
+        function closeDialog()
+        {
+            $mdDialog.hide();
+        }
+    }
+})();
