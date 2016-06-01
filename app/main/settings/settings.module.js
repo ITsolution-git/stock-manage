7f2f04d718e6fb99c10d252b56b4ab3e47028769
tshobhasana <tshobhasana@codal.com>
2016-06-01 14:57:51 +0530

    settings.module.js

--- app/main/settings/settings.module.js
+++ app/main/settings/settings.module.js
@@ -9,111 +9,20 @@
     /** @ngInject */
     function config($stateProvider, $translatePartialLoaderProvider, msApiProvider, msNavigationServiceProvider)
     {
-        // State
-        $stateProvider
-            .state('app.settings', {
-                url    : '/settings',
-                views  : {
-                    'content@app': {
-                        templateUrl: 'app/main/settings/settings.html',
-                        controller : 'SettingsController as vm'
-                    }
-                },
-                resolve: {
-                    
-                }
-            }).state('app.settings.userProfile', {
-                resolve: {
-                    checksession : function (sessionService,$state)
-                    {
-                       return sessionService.AccessService('ALL');
-                    },
-                },
-                url  : '/userProfile',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/settings/views/userProfile/userProfile.html',
-                        controller : 'UserProfileController as vm'
-                    }
-                }
-            }).state('app.settings.priceGrid', {
-                url  : '/priceGrid',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/settings/views/priceGrid/priceGrid.html',
-                        controller : 'PriceGridController as vm'
-                    }
-                },resolve: {
-                   
-                }
-            }).state('app.settings.companyProfile', {
-                resolve: {
-                    checksession : function (sessionService,$state)
-                    {
-                       return sessionService.AccessService('CA');
-                    },
-                },
-                url  : '/companyProfile',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/settings/views/companyProfile/companyProfile.html',
-                        controller : 'UserProfileController as vm'
-                    }
-                }
-            }).state('app.settings.companyDetails', {
-                url  : '/companyDetails',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/settings/views/companyDetails/companyDetails.html',
-                        controller : 'CompanyDetailsController as vm'
-                    }
-                },resolve: {
-                   
-                }
-            }).state('app.settings.userManagement', {
-                url  : '/userManagement',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/settings/views/userManagement/userManagement.html',
-                        controller : 'UserManagementController as vm'
-                    }
-                },resolve: {
-                   
-                }
-            }).state('app.settings.affiliate', {
-                url  : '/affiliate',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/settings/views/affiliate/affiliate.html',
-                        controller : 'AffiliateController as vm'
-                    }
-                },resolve: {
-                   
-                }
-            }).state('app.settings.support', {
-                url  : '/support',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/settings/views/support/support.html',
-                        controller : 'SupportController as vm'
-                    }
-                },resolve: {
-                   
-                }
-            })
-            ;
+        
 
         // Navigation
-        msNavigationServiceProvider.saveItem('fuse', {
+       /* msNavigationServiceProvider.saveItem('fuse', {
             title : '',
             group : true,
             weight: 1
-        });
+        });*/
 
         msNavigationServiceProvider.saveItem('fuse.settings', {
             title    : 'Settings',
             icon     : 'icon-cog',
             //state    : 'app.settings',
+            
             weight   : 1
         });
 
@@ -121,6 +30,7 @@
             title      : 'User Profile',
             state      : 'app.settings.userProfile',
             stateParams: {'id': 1},
+            
             weight     : 1
         });
 
@@ -128,6 +38,7 @@
             title      : 'Company Profile',
             state      : 'app.settings.companyProfile',
             stateParams: {'id': 2},
+            
             weight     : 2
         });
 
@@ -135,6 +46,7 @@
             title      : 'Company Details',
             state      : 'app.settings.companyDetails',
             stateParams: {'id': 3},
+            
             weight     : 3
         });
 
@@ -142,38 +54,49 @@
             title      : 'User Management',
             state      : 'app.settings.userManagement',
             stateParams: {'id': 4},
+            
             weight     : 4
         });
         msNavigationServiceProvider.saveItem('fuse.settings.priceGrid', {
             title      : 'Price Grid',
             state      : 'app.settings.priceGrid',
             stateParams: {'id': 5},
+            
             weight     : 5
         });
         msNavigationServiceProvider.saveItem('fuse.settings.affiliate', {
             title      : 'Affiliate',
             state      : 'app.settings.affiliate',
             stateParams: {'id': 6},
+            
             weight     : 6
         });
-        msNavigationServiceProvider.saveItem('fuse.settings.xyadz', {
+        msNavigationServiceProvider.saveItem('fuse.settings.integrations', {
             title      : 'Integrations',
-            state      : 'app.settings.xyadz',
+            state      : 'app.settings.integrations',
             stateParams: {'id': 7},
+            
             weight     : 7
-        });
+        });/*
         msNavigationServiceProvider.saveItem('fuse.settings.support', {
             title      : 'Support',
             state      : 'app.settings.support',
             stateParams: {'id': 8},
             weight     : 8
         });
-        msNavigationServiceProvider.saveItem('fuse.settings.xybdz', {
+        msNavigationServiceProvider.saveItem('fuse.settings.billing', {
             title      : 'Billing',
-            state      : 'app.settings.xybdz',
+            state      : 'app.settings.billing',
             stateParams: {'id': 9},
             weight     : 9
         });
+
+        msNavigationServiceProvider.saveItem('fuse.settings.platformSettings', {
+            title      : 'Platform Settings',
+            state      : 'app.settings.platformSettings',
+            stateParams: {'id': 10},
+            weight     : 10
+        });*/
 
 
     }
