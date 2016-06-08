abee2a7e946a3ec1f2944279ba5d5ac4119e55f1
hdeliwala <hdeliwala@codal.com>
2016-06-08 19:40:11 +0530

    STO-220 #comment working on edit functionality in price grid  #time 2h working on edit functionality in price grid

--- app/main/settings/views/priceGrid/createPriceGrid.controller.js
+++ app/main/settings/views/priceGrid/createPriceGrid.controller.js
@@ -88,5 +88,97 @@
 
         $scope.priceDetail();
 
+            $scope.savePrice = function(price,price_grid,price_primary,price_secondary,garment_mackup,garment,embroswitch,allEmbroidery) {
+                   
+                    var combine_array_data = {};
+                    combine_array_data.price = price;
+                    combine_array_data.price_grid = price_grid;
+                    combine_array_data.price_primary = price_primary;
+                    combine_array_data.price_secondary = price_secondary;
+                    combine_array_data.garment_mackup = garment_mackup;
+                    combine_array_data.garment = garment;
+                    combine_array_data.embroswitch = embroswitch;
+                    combine_array_data.allEmbroidery = allEmbroidery;
+
+                   if(price.id) {
+                         
+                    $http.post('api/public/admin/priceEdit',combine_array_data).success(function(result, status, headers, config) {
+  
+                      if(result.data.success == '1') {
+                         window.history.back();
+                       } 
+                   
+                    });
+                    
+                   } else {
+                  
+                      $state.go('app.dashboard');
+                      return false;
+
+                   }
+
+                   };
+
+                    $scope.allPriceGrid = [];
+                    $scope.addInput = function(){
+                      $scope.allPriceGrid.push({item:'', time:'', charge:'', is_gps_distrib:'', is_gps_opt:'', is_per_line:'', is_per_order:'', is_per_screen_set:''});
+                    }
+
+                    $scope.removeInput = function(index){
+                        $scope.allPriceGrid.splice(index,1);
+                    }
+
+
+                    $scope.allScreenPrimary = [];
+                    $scope.addScreenPrimary = function(){
+                      $scope.allScreenPrimary.push({range_low:'', range_high:'', pricing_1c:'', pricing_2c:'', pricing_3c:'', pricing_4c:'', pricing_5c:'', pricing_6c:'',pricing_7c:'',pricing_8c:'',pricing_9c:'',pricing_10c:'',pricing_11c:'',pricing_12c:''});
+                    }
+
+                    $scope.removeScreenPrimary = function(index){
+                        $scope.allScreenPrimary.splice(index,1);
+                    }
+
+                    $scope.allScreenSecondary = [];
+                    $scope.addScreenSecondary = function(){
+                      $scope.allScreenSecondary.push({range_low:'', range_high:'', pricing_1c:'', pricing_2c:'', pricing_3c:'', pricing_4c:'', pricing_5c:'', pricing_6c:'',pricing_7c:'',pricing_8c:'',pricing_9c:'',pricing_10c:'',pricing_11c:'',pricing_12c:''});
+                    }
+
+                    $scope.removeScreenSecondary = function(index){
+                        $scope.allScreenSecondary.splice(index,1);
+                    }
+
+
+                     $scope.allGarment = [];
+                    $scope.addGarment = function(){
+                      $scope.allGarment.push({range_low:'', range_high:'', pricing_1c:'', pricing_2c:'', pricing_3c:'', pricing_4c:'', pricing_5c:'', pricing_6c:'',pricing_7c:'',pricing_8c:'',pricing_9c:'',pricing_10c:'',pricing_11c:'',pricing_12c:''});
+                    }
+
+                    $scope.removeGarment = function(index){
+                        $scope.allGarment.splice(index,1);
+                    }
+
+
+                     $scope.allGarmentMackup = [];
+                    $scope.addGarmentMackup = function(){
+                      $scope.allGarmentMackup.push({range_low:'', range_high:'', percentage:''});
+                    }
+
+                    $scope.removeGarmentMackup = function(index){
+                        $scope.allGarmentMackup.splice(index,1);
+                    }
+
+                      $scope.allEmbroidery = [];
+                    $scope.addEmbroidery = function(){
+                      $scope.allEmbroidery.push({range_low:'', range_high:'', pricing_1c:'', pricing_2c:'', pricing_3c:'', pricing_4c:'', pricing_5c:'', pricing_6c:'',pricing_7c:'',pricing_8c:'',pricing_9c:'',pricing_10c:'',pricing_11c:'',pricing_12c:''});
+                    }
+
+                    $scope.removeEmbroidery = function(index){
+                        $scope.allEmbroidery.splice(index,1);
+                    }
+
+                  $scope.cancel = function() {
+                         window.history.back();
+                  }
+
     }
 })();
