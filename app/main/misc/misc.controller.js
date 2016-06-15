8a0a7791894d80aa7b1fe18c48c57faf48fbab7d
hdeliwala <hdeliwala@codal.com>
2016-06-15 11:26:51 +0530

    STO-231 #comment value 1 listing page in misc data tab  #time 2h value 1 listing page in misc data tab

--- app/main/misc/misc.controller.js
+++ app/main/misc/misc.controller.js
@@ -5,12 +5,44 @@
             .module('app.misc')
             .controller('miscController', miscController);
     /** @ngInject */
-    function miscController(miscData, $q, $mdDialog, $document, $mdSidenav, DTOptionsBuilder, DTColumnBuilder) {
+    function miscController($q, $mdDialog, $document, $mdSidenav, DTOptionsBuilder, DTColumnBuilder,sessionService,$http,$scope) {
         var vm = this;
-        vm.searchQuery = "";
         
-        // Data
-        vm.ows = miscData.miscDetail.data;
-        vm.oip = miscData.miscDetail.data1;
+        $("#ajax_loader").show();
+
+ var company_id = sessionService.get('company_id');
+
+ var misc_list_data = {};
+ var condition_obj = {};
+ condition_obj['company_id'] =  company_id;
+ misc_list_data.cond = angular.copy(condition_obj);
+
+ $http.post('api/public/common/getAllMiscData',misc_list_data).success(function(result, status, headers, config) {
+             
+              $scope.miscData = result.data.records;
+             // $scope.pagination = AllConstant.pagination;
+              $("#ajax_loader").hide();    
+      });
+
+
+var range = [];
+for(var i=0;i<17;i++) {
+  range.push(i);
+}
+$scope.range = range;
+
+
+/*$('.footable-page a').filter('[data-page="0"]').trigger('click');
+
+$scope.updateUser = function(value,id) {
+
+              var combine_array_data = {};
+              combine_array_data.value = value;
+              combine_array_data.id = id;
+
+               $http.post('api/public/admin/miscSave',combine_array_data).success(function(result, status, headers, config) {
+                         
+                          });
+  };*/
     }
 })();
--- app/main/misc/misc.module.js
+++ app/main/misc/misc.module.js
@@ -18,52 +18,13 @@
                         templateUrl: 'app/main/misc/misc.html',
                         controller : 'miscController as vm'
                     }
-                },
-                resolve: {
-                    miscData: function (msApi)
-                    {
-                        return msApi.resolve('misc@get');
-                    }
-                }
-            }).state('app.misc.orderwaitship', {
-                url  : '/orderwaitship',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/misc/views/orderwaitship/orderwaitship.html',
-                        controller : 'orderWaitController as vm'
-                    }
-                }
-            }).state('app.misc.shipmentdetails', {
-                url  : '/shipmentdetails',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/misc/views/shipmentdetails/shipmentdetails.html',
-                        controller : 'shipmentController as vm'
-                    }
-                }
-            }).state('app.misc.boxingdetail', {
-                url  : '/boxingdetail',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/misc/views/boxingdetail/boxingdetail.html',
-                        controller : 'boxingdetailController as vm'
-                    }
-                }
-            }).state('app.misc.shipmentoverview', {
-                url  : '/shipmentoverview',
-                views: {
-                    'content@app': {
-                        templateUrl: 'app/main/misc/views/shipmentoverview/shipmentoverview.html',
-                        controller : 'shipmentOverviewController as vm'
-                    }
                 }
             });
 
        // Translation
         $translatePartialLoaderProvider.addPart('app/main/misc');
 
-        // Api
-        msApiProvider.register('misc', ['app/data/misc/misc.json']);
+       
         // Navigation
         msNavigationServiceProvider.saveItem('fuse', {
             title : '',
