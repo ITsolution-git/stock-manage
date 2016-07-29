c25be8ec18195e0d8b71a821d8748f99336021fd
kjoshi <kjoshi@codal.com>
2016-07-29 15:03:35 +0530

    STO-297 #time 1h screen count calculation set.

--- api/app/Art.php
+++ api/app/Art.php
@@ -68,7 +68,7 @@ class Art extends Model {
     public function ScreenSets($post) // ART SCREEN DETAIL PAGE FOR SCREEN SETS
 	{
 		$query = DB::table('artjob_screensets as ass')
-				->select('or.name as order_name','or.created_date','cc.first_name','cc.last_name','cl.client_id','cl.client_company','mt.value as position_name','ass.screen_count','ass.screen_set','ass.id as screen_id','odp.color_stitch_count','ass.frame_size','ass.line_per_inch','ass.screen_width','ass.screen_height','ass.screen_location','ass.screen_active','ass.order_id')
+				->select('or.name as order_name','or.created_date','cc.first_name','cc.last_name','cl.client_id','cl.client_company','mt.value as position_name','ass.screen_count','ass.screen_set','ass.id as screen_id','odp.color_stitch_count','ass.frame_size','ass.line_per_inch','ass.screen_width','ass.screen_height','ass.screen_location','ass.screen_active','ass.order_id',DB::raw("(odp.color_stitch_count+odp.foil_qnty) as screen_total"))
 				->join('art as art','art.order_id','=','ass.order_id')
 				->join('orders as or','art.order_id','=','or.id')
 				->Join('client as cl', 'cl.client_id', '=', 'or.client_id')
@@ -179,7 +179,7 @@ class Art extends Model {
     public function GetScreenset_detail($position_id)
     {
     	$query = DB::table('artjob_screensets as ass')
-				->select('ord.id as order_id','od.id as design_id','odp.color_stitch_count','mt.value','ass.*')
+				->select(DB::raw("(odp.color_stitch_count+odp.foil_qnty) as screen_total"),'ord.id as order_id','od.id as design_id','odp.color_stitch_count','mt.value','ass.*')
 				->join('order_design_position as odp','odp.id','=','ass.positions')
 				->join('order_design as od','odp.design_id','=','od.id')
 				->join('orders as ord','ord.id','=','od.order_id')
