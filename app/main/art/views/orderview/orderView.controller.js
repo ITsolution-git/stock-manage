c835e005c898e2beb5170f502e0c016897f21fef
kjoshi <kjoshi@codal.com>
2016-08-02 17:25:20 +0530

    STO-305 #time 8h edit color screen with thread color auto suggest complete.

--- app/main/art/views/orderview/orderView.controller.js
+++ app/main/art/views/orderview/orderView.controller.js
@@ -35,20 +35,6 @@
         $scope.GetOrderScreenSet();
 
 
-        $scope.screensetPOinfo  = {
-            "client": "Client Name",
-            "orderName": "12345",
-            "orderDate": "12/05/2016",
-            "contract": "Joshi Goodman",
-            "affiliate": "Affiliate Name",
-            "affiliateArrival": "xx/xx/xxxx",
-            "affiliateDeadline": "xx/xx/xxxx"
-        };
-        vm.screensetinfo = [
-            {"Position": "Front", "NumberColors": "30", "FrameSize": "32", "Width": "10", "PrintLocation": "Top", "NumberScreens": "10", "LinesPerInch": "10", "Height": "20"},
-            {"Position": "Front", "NumberColors": "30", "FrameSize": "32", "Width": "10", "PrintLocation": "Top", "NumberScreens": "10", "LinesPerInch": "10", "Height": "20"},
-            {"Position": "Front", "NumberColors": "30", "FrameSize": "32", "Width": "10", "PrintLocation": "Top", "NumberScreens": "10", "LinesPerInch": "10", "Height": "20"}
-        ];
         function createNewScreen(ev, position_id) {
 
             $mdDialog.show({
@@ -178,7 +164,7 @@
                 }
             });
         }
-        //        Datatable Options
+        // Datatable Options
        
         var originatorEv;
         vm.openMenu = function ($mdOpenMenu, ev) {
