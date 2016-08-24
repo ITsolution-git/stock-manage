5948c4f0e605d285c28319ad032cbf309220293c
hdeliwala <hdeliwala@codal.com>
2016-08-24 13:19:43 +0530

    make connection dynamic for quickbook

--- app/core/directives/ConnectToQuickBooksDirective.js
+++ app/core/directives/ConnectToQuickBooksDirective.js
@@ -2,10 +2,11 @@
 
 angular.module('app.core')
 
-.directive('connectToQuickbooks', function($window){
+.directive('connectToQuickbooks', function($window,$location,$browser){
   return {
     restrict: 'E',    template: "<ipp:connectToIntuit></ipp:connectToIntuit>",
     link: function(scope) {
+        var base_path = "http://"+$location.host()+$browser.baseHref();
         var script = $window.document.createElement("script");
         script.type = "text/javascript";
         script.src = "//js.appcenter.intuit.com/Content/IA/intuit.ipp.anywhere.js";
@@ -15,8 +16,9 @@ angular.module('app.core')
         $window.document.body.appendChild(script);
         scope.$on('intuitjs:loaded', function (evt) {
           $window.intuit.ipp.anywhere.setup({
-            menuProxy: 'http://localhost/stokkup/api/vendor/consolibyte/quickbooks/docs/partner_platform/example_app_ipp_v3/menu.php',
-            grantUrl: 'http://localhost/stokkup/api/public/qbo/oauth'
+
+            menuProxy: base_path+'api/vendor/consolibyte/quickbooks/docs/partner_platform/example_app_ipp_v3/menu.php',
+            grantUrl: base_path+'api/public/qbo/oauth'
            });
           scope.connected = true;
           scope.$apply();
