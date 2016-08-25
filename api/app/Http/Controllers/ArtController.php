e406e6d09e84e823f514ecf19b1d04a9769d2ff9
kjoshi <kjoshi@codal.com>
2016-08-25 17:28:08 +0530

    email position change.

--- api/app/Http/Controllers/ArtController.php
+++ api/app/Http/Controllers/ArtController.php
@@ -484,7 +484,7 @@ class ArtController extends Controller {
            
                 $pdf_url = "ScreenApproval-".$screenArray->order_id.".pdf"; 
                 $filename = $file_path."/". $pdf_url;
-                PDF::Output($filename);
+                PDF::Output($filename,'F');
 
                 if(!empty($screenArray->mail) && $screenArray->mail=='1' && !empty($pdf_data[0][0]->billing_email))
                 {
@@ -495,7 +495,7 @@ class ArtController extends Controller {
                     });
                 }
 
-                //return Response::download($filename);
+                return Response::download($filename);
 
             }
             else
