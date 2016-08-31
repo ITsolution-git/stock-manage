b6ad75db3de30c62b09bc351fb5719757f0ceb68
pdave@codal.com <piyushdave>
2016-08-31 16:05:57 +0530

    SB-34 #comment added order status in all module #time 4h added order status in all module

--- api/app/Shipping.php
+++ api/app/Shipping.php
@@ -146,7 +146,7 @@ class Shipping extends Model {
     public function shippingDetail($data) {
 
         $whereShippingConditions = ['s.id' => $data['shipping_id']];
-        $listArray = ['s.id as shipping_id','mt.value as job_status','o.id as order_id','o.name','cd.id as client_distribution_id','o.client_id','c.client_company',
+        $listArray = ['s.id as shipping_id','mt.value as job_status','o.id as order_id','o.name','cd.id as client_distribution_id','o.client_id','c.client_company','o.approval_id',
                         's.boxing_type','o.shipping_by','o.in_hands_by','s.shipping_type_id','o.date_shipped','o.fully_shipped','s.shipping_note','s.cost_to_ship','cd.*','o.f_approval','s.sku'];
 
         $shippingData = DB::table('shipping as s')
