27f41ce162b4776f24051480e3407c05238dc03b
hdeliwala <hdeliwala@codal.com>
2016-09-02 16:22:15 +0530

    SB-84 #comment Auto search functionality in create order  #time 4h Auto search functionality in create order

--- app/main/order/dialogs/order/order-dialog.controller.js
+++ app/main/order/dialogs/order/order-dialog.controller.js
@@ -9,14 +9,19 @@
     function OrderDialogController($scope, $mdDialog, $document, $mdSidenav, DTOptionsBuilder, DTColumnBuilder,$resource,$http,notifyService,$state,sessionService)
     {
         var companyData = {};
-            companyData.cond ={company_id :sessionService.get('company_id'),is_delete :'1',status :'1'};
-            companyData.table ='client';
+            companyData.company_id =sessionService.get('company_id');
 
-                $http.post('api/public/common/GetTableRecords',companyData).success(function(result) {
-        
+                $http.post('api/public/order/GetAllClientsLowerCase',companyData).success(function(result) {
+       
                         if(result.data.success == '1') 
                         {
                             $scope.allCompany =result.data.records;
+
+                             $scope.simulateQuery = false;
+                              $scope.isDisabled    = false;
+                              $scope.states        = loadAll();
+                              $scope.querySearch   = querySearch;
+
                         } 
                         else
                         {
@@ -25,9 +30,38 @@
                 });
 
 
+                function querySearch (query) 
+                                    {
+                                        var results = query ? $scope.states.filter( createFilterFor(query) ) : $scope.states, deferred;
+                                        if ($scope.simulateQuery) 
+                                        {
+                                            deferred = $q.defer();
+                                            $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+                                            return deferred.promise;
+                                        } 
+                                        else 
+                                        {
+                                            return results;
+                                        }
+                                    }
+                                    function loadAll() 
+                                    {
+                                        var allStates = $scope.allCompany;
+                                        return allStates;
+                                    }
+                                    function createFilterFor(query) 
+                                    {
+                                        var lowercaseQuery = angular.lowercase(query);
+                                        return function filterFn(state) 
+                                        {
+                                            return (state.client_company.indexOf(lowercaseQuery) === 0);
+                                        };
+                                    }
+
+
                  $scope.save = function (orderData) {
- 
-          
+                  
+              
                    if(orderData == undefined) {
 
                       var data = {"status": "error", "message": "Company and Job Name should not be blank"}
@@ -38,7 +72,7 @@
                       var data = {"status": "error", "message": "Name should not be blank"}
                               notifyService.notify(data.status, data.message);
                               return false;
-                    } else if(orderData.client_id == undefined) {
+                    } else if(orderData.client == null) {
 
                       var data = {"status": "error", "message": "Company should not be blank"}
                               notifyService.notify(data.status, data.message);
@@ -50,6 +84,8 @@
               combine_array_id.orderData = orderData;
               combine_array_id.company_id = sessionService.get('company_id');
               combine_array_id.login_id = sessionService.get('user_id');
+
+              
 
               $http.post('api/public/order/addOrder',combine_array_id).success(function(result) 
                 {
