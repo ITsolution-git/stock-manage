30520aea5ee806cb7a9f53a740321cc2d658cb9a
kjoshi <kjoshi@codal.com>
2016-09-20 18:10:41 +0530

    SB-120 #time 20m Streent number condition change.

--- app/main/client/views/profile/profile-view.controller.js
+++ app/main/client/views/profile/profile-view.controller.js
@@ -585,8 +585,8 @@
 
         $scope.GetAPIData = function (apidata)
         {
-            $scope.client.pl_address = angular.isUndefined(apidata.streetNumber)?'':apidata.streetNumber;
-            $scope.client.pl_address = angular.isUndefined(apidata.street)?$scope.client.pl_address:$scope.client.pl_address+", "+apidata.street;
+            $scope.client.pl_address = angular.isUndefined(apidata.streetNumber)?'':apidata.streetNumber+", ";
+            $scope.client.pl_address = angular.isUndefined(apidata.street)?$scope.client.pl_address:$scope.client.pl_address+apidata.street;
             //$scope.client.pl_suite = angular.isUndefined(apidata.streetNumber)?'':apidata.streetNumber;
             $scope.client.pl_city = angular.isUndefined(apidata.city)?'':apidata.city;
             for(var i=0; i<$scope.states_all.length; i++)
