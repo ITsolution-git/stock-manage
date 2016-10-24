27e198fe01349b244b1da52f22627c6cd82eedce
Bhargav Pithva <bpithva@codal.com>
2016-10-24 17:33:26 +0530

    SB-335 #time 10m set json error message

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -19,14 +19,18 @@ abstract class Controller extends BaseController {
     	$common = new Common();
     	$this->common = $common;
         $headers = Request::header('Authorization');
-
 		if (!empty($headers)){
 			$token_data = $this->common->GetTableRecords('login_token',array('token' => $headers),array(),0,0,'token');
 			if (empty($token_data)) {
-				echo json_encode(['message' => 'Not valid token']); exit;
+				$message = "Invalid Token";
+          		$data = json_encode(array("data"=>["success"=>0,'message' =>$message]));
+          		print_r($data);exit;
 			}
 		}else{
-				echo json_encode(['message' => 'Not valid token']); exit;
+				$data = array("success"=>0,'message' =>'Invalid Token');
+            	$message = "Invalid Token";
+          		$data = json_encode(array("data"=>["success"=>0,'message' =>$message]));
+          		print_r($data);exit;
 		}
     }
 }
