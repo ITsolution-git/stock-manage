39efb1cfc7017d963fcd3301ed86bb0aee77954f
Codal-Kavi <kjoshi@codal.com>
2016-12-01 12:41:48 +0530

    SB-452 SB-456 #time 8h set API for weekely and Machine tab

--- api/app/Production.php
+++ api/app/Production.php
@@ -255,18 +255,77 @@ class Production extends Model {
                     ->leftJoin('orders as ord','ord.id','=','od.order_id')
                     ->where('od.company_id','=',$company_id)
                     ->where('ps.run_date','=',$run_date)
-                    ->where('ps.run_date','=',$run_date)
                     ->where('od.is_delete','=','1')
                     ->where('odp.is_delete','=','1')
+                    ->orderBy('ord.id','desc')
+                    ->orderBy('odp.id','desc')
                     ->get();
 
         $ret_array = array();            
         foreach($result as $key=>$value)
         {
-            
             $ret_array[$value->machine_id]['machine_name'] = $value->machine_name;
             $ret_array[$value->machine_id]['machine_data'][$value->shift_id]['shift_name']=$value->shift_name;
             $ret_array[$value->machine_id]['machine_data'][$value->shift_id]['shift_data'][$value->position_id]=$value;
+        }            
+        return $ret_array;
+    }
+    public function SchedualBoardweekData($company_id,$start_date,$end_date)
+    {
+        //echo $start_date."=".$end_date; die();
+        $result = DB::table('position_schedule as ps')
+                    ->select('cs.shift_name','mc.machine_name','odp.id as position_id','mt.value as position_name','ord.display_number','ord.name','ps.*')
+                    ->leftjoin('company_shift as cs','cs.id','=','ps.shift_id')
+                    ->leftjoin('machine as mc','mc.id','=','ps.machine_id')
+                    ->leftjoin('order_design_position as odp','ps.position_id','=','odp.id')
+                    ->leftjoin('misc_type as mt','mt.id','=','odp.position_id')
+                    ->leftJoin('order_design as od','od.id','=','odp.design_id')
+                    ->leftJoin('orders as ord','ord.id','=','od.order_id')
+                    ->where('od.company_id','=',$company_id)
+                    ->whereBetween('ps.run_date', [$start_date, $end_date])
+                    ->where('od.is_delete','=','1')
+                    ->where('odp.is_delete','=','1')
+                    ->orderBy('ps.run_date','asc')
+                    ->orderBy('ord.id','desc')
+                    ->orderBy('odp.id','desc')
+                    ->get();
+
+        $ret_array = array();            
+        foreach($result as $key=>$value)
+        {
+            $ret_array[$value->run_date]['date'] = date('l - m/d/Y',strtotime($value->run_date));
+            $ret_array[$value->run_date]['date_data'][$value->shift_id]['shift_name']=$value->shift_name;
+            $ret_array[$value->run_date]['date_data'][$value->shift_id]['shift_data'][$value->position_id]=$value;
+        }            
+        return $ret_array;
+    }
+
+    public function SchedualBoardMachineData($company_id,$run_date,$machine_id)
+    {
+        $where = (!empty($machine_id))?array('ps.machine_id'=>$machine_id):array();
+        $result = DB::table('position_schedule as ps')
+                    ->select('cs.shift_name','mc.machine_name','odp.id as position_id','mt.value as position_name','ord.display_number','ord.name','ps.*')
+                    ->leftjoin('company_shift as cs','cs.id','=','ps.shift_id')
+                    ->leftjoin('machine as mc','mc.id','=','ps.machine_id')
+                    ->leftjoin('order_design_position as odp','ps.position_id','=','odp.id')
+                    ->leftjoin('misc_type as mt','mt.id','=','odp.position_id')
+                    ->leftJoin('order_design as od','od.id','=','odp.design_id')
+                    ->leftJoin('orders as ord','ord.id','=','od.order_id')
+                    ->where('od.company_id','=',$company_id)
+                    ->where('ps.run_date','=',$run_date)
+                    ->where('od.is_delete','=','1')
+                    ->where($where)
+                    ->where('odp.is_delete','=','1')
+                    ->orderBy('ord.id','desc')
+                    ->orderBy('odp.id','desc')
+                    ->get();
+
+        $ret_array = array();            
+        foreach($result as $key=>$value)
+        {
+            $ret_array['shift_all'][]=$value;
+            $ret_array['shifts'][$value->shift_id]['shift_name']=$value->shift_name;
+            $ret_array['shifts'][$value->shift_id]['shift_data'][$value->position_id]=$value;
         }            
         return $ret_array;
     }
