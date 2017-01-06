378ef62e98c4dacb759b5e14c73d8a9a477baf1c
pdave@codal.com <piyushdave>
2017-01-06 13:14:07 +0530

    SB-560 #comment given functionality to edit tracking number while freight shipping #time 1h

--- app/main/shipping/views/shipmentoverview/shipmentoverview.controller.js
+++ app/main/shipping/views/shipmentoverview/shipmentoverview.controller.js
@@ -306,5 +306,26 @@
             document.body.appendChild(form);
             form.submit();
         }
+
+        $scope.UpdateBoxData = function(value,id)
+        {
+            var box_data = {};
+            box_data.table ='shipping_box';
+            $scope.name_filed = 'tracking_number';
+            
+            var obj = {};
+            obj[$scope.name_filed] =  value;
+            box_data.data = angular.copy(obj);
+            var condition_obj = {};
+            
+            condition_obj['id'] =  id;
+            box_data.cond = angular.copy(condition_obj);
+
+            $http.post('api/public/common/UpdateTableRecords',box_data).success(function(result) {
+
+                var data = {"status": "success", "message": "Data Updated Successfully."}
+                notifyService.notify(data.status, data.message);
+            });
+        }
     }
 })();
